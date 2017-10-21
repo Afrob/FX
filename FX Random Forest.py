c118f8b4cd1afae3dbd9f1755812b4235b4054d0
Afrob <32912939+Afrob@users.noreply.github.com>
2017-10-21 13:57:53 +0200

    finale Version

--- FX Random Forest.py
+++ FX Random Forest.py
@@ -10,27 +10,34 @@ from IPython.display import display, Image
 import pydotplus
 import math
 import random
+import time
 
-nAlpha=12   #Anzahl Random Forest für CV bei der Optimierung auf Alpha
-t=1000000   #Länge der Daten im Decision Tree (z.B. Anzahl Candles für 1 Monat)
+starttime=time.time()
+
+nAlpha=20   #Anzahl Random Forest für CV bei der Optimierung auf Alpha
+t=1432   #Länge der Daten im Decision Tree (z.B. Anzahl Candles für 1 Monat)
 
 
-M=pd.read_csv("USDZAR_prepared_Ind.csv")
+M=pd.read_csv("USDZAR-XAUUSD3M_prepared_Ind.csv",index_col='Date')
 
 N=len(M.index)
-nForest=((N/t)-nAlpha+1)/1.2        #Anzahl Bäume im Random Forest
-nTree=nForest+nAlpha-1              #Anzahl aller Bäume über gesamte Datenset
-KForest=nForest*t/5                 #Größe Testset für Random Forest. Für Trees KTree=t
+nForest=math.floor(((N/t)-nAlpha+1)/1.2 )        #Anzahl Bäume im Random Forest
+nTree=nForest+nAlpha-1                          #Anzahl aller Bäume über gesamte Datenset
+KForest=math.floor(nForest*t/5)                  #Größe Testset für Random Forest. Für Trees KTree=t
 
 
 Tree={}
 mse={}
-LowLeaves=5
-HighLeaves=1000
+ErrIn={}
+maxdepth={}
+LowLeaves=1
+HighLeaves=10000
 
 
 #Bilden der Bäume über alle Daten
-for i in np.arange(0,nTree,1):
+i=0
+while i<nTree:
+    print(i, '/',nTree)
     Train=M[(N-KForest)-i*t-t:(N-KForest)-i*t]          #Trainingsset der Bäume
     Test=M[(N-KForest)-i*t:(N-KForest)-i*t+t+1]         #Testset der Bäume hat Größe t
     XTrain=Train[Train.columns[:len(Train.columns)-1]]  #Parameter des Trainigssets
@@ -38,17 +45,31 @@ for i in np.arange(0,nTree,1):
     XTest=Test[Test.columns[:len(Test.columns)-1]]      #Parameter des Testsets
     YTest=Test[Test.columns[len(Test.columns)-1]]       #Zielwert des Testsets
 
-        #Bilden der Bäume über verschiedene Anzahl an Leaves
-        for depth in np.arange(LowLeaves,HighLeaves,1):
-            DTree=DecisionTreeRegressor(criterion="mse", max_depth=depth)
-            Tree[i,depth]=DTree.fit(XTrain,YTrain)
-            YPredict=Tree[i,depth].predict(XTrain)
-            mse[i,depth]=mean_squared_error(YPredict,YTest)
+    #Bilden der Bäume über verschiedene Anzahl an Leaves
+    depth=LowLeaves
+    j=0
+    while depth <= HighLeaves:
+        print(i, '/',nTree,',',depth,'/',HighLeaves)
+        DTree=DecisionTreeRegressor(criterion="mse", max_depth=depth)
+        #DTree = DecisionTreeRegressor(criterion="mse", max_leaf_nodes=depth)
+        Tree[i,depth]=DTree.fit(XTrain,YTrain)
+        YPredict=Tree[i,depth].predict(XTest)
+        mse[i,depth]=mean_squared_error(YPredict,YTest)
+        YInPredict=Tree[i,depth].predict(XTrain)
+        ErrIn[i,depth]=mean_squared_error(YInPredict,YTrain)
+        if depth>LowLeaves:
+            if Tree[i,depth].tree_.node_count==Tree[i,depth-1].tree_.node_count:
+                j=j+1
+        if j==3:
+            maxdepth[i]=depth
+            depth=HighLeaves
+        depth=depth+1
+    i=i+1
 
 #Ermitteln des optimalen Lambda
 lb=0                    #untere Grenze des Lambda
-hb=0.00001                  #obere Grenze des Lambda
-StepSize=0.000001        #erste Schrittgröße, in der das Lambda gesucht werden soll
+hb=100                  #obere Grenze des Lambda
+StepSize=1              #erste Schrittgröße, in der das Lambda gesucht werden soll
 ErrCVStar=math.inf      #unendlich
 ErrCVCheck=math.inf     #unendlich
 lamb=lb                 #Anfangswert des Lambda
@@ -56,79 +77,104 @@ hbneu=hb
 epsilon=1
 j=0
 depthlamb={}
-while j<3:
+while j<=3:
     #Durchgehen der einzelnen Lambdas und ermitteln der jeweiligen ErrCV
     while lamb <= hbneu:
-        if epsilon==0:
+        print(lamb)
+        if epsilon==1:
             Rlamb=lamb
         else:
             # Um nicht in lokalem Minimun zu landen Random Nummer aus alter Range
-            Rlamb=np.random.choice([lamb,random.choice(np.arange(lb,hb,StepSize))],p=[1-1/epsilon,1/epsilon])
+            Rlamb=np.random.choice([lamb,random.uniform(lb,hb)],p=[1-1/epsilon,1/epsilon])
         if Rlamb==lamb:
             lamb=lamb+StepSize
 
         ErrCV=0
         #Durchgehen der einzelnen Bäume mit dem aktuellen Lambda
-        for i in np.arange(0,nForest,1):
+        i=0
+        while i<nForest:
+            print('Lambda:',Rlamb,'/',hbneu,' by StepSize ',StepSize,',',i,'/',nForest)
             ErrAugStar= math.inf    #unendlich
             #Anzahl der Leaves mit dem geringsten ErrAug ermitteln
-            for depth in np.arange(LowLeaves,HighLeaves,1):
-                ErrAug=mse[i,depth]+Rlamb*depth
+            depth=LowLeaves
+            while depth <=maxdepth[i]:
+                ErrAug=ErrIn[i,depth]+Rlamb*depth
                 #Auswahl des Baumes mit dem geringsten ErrAug
                 if ErrAug < ErrAugStar:
                     ErrAugStar=ErrAug
                     mseTree=mse[i,depth]
                     depthlamb[i]=depth
-                    #hier und in ErrCVStar schon Baumauswahl rein: direkt über Tree??? oder ilamb bzw. jlamb/ilambstar...?
+                depth = depth + 1
 
             #Ermitteln des ErrCV für das jeweilige Lambda
             ErrCV=ErrCV+mseTree
+            i=i+1
 
         #Speichern des Lambda und der Bäume mit dem geringsten ErrCV
-        if ErrCV<ErrCVStar:
+        if ErrCV/nForest<ErrCVStar:
             lambStar=Rlamb
-            ErrCVStar=ErrCV
+            ErrCVStar=ErrCV/nForest
             depthstar=depthlamb
 
     #Zähler, ob eine weitere Nachkommastelle sich lohnt
-    if ErrCVCheck - ErrCVStar < 0.00000001:
+    if ErrCVCheck - ErrCVStar < 0.1:
         j = j + 1
-        ErrCVCheck=ErrCVStar
     else:
         j = 0
+    ErrCVCheck=ErrCVStar
 
     #Anpassen der neuen Grenzen und der neuen StepSize
     lamb=max(0,lambStar-StepSize)
     hbneu=lambStar+StepSize
     StepSize = StepSize / 10
-    Epsilon=Epsilon+1
+    epsilon=epsilon+1
+
+#Depth für restliche Bäume aus nAlpha ermitteln
+i=nForest
+while i < nTree:
+    print('Lambda:', lambStar, ',', i, '/', nTree)
+    ErrAugStar = math.inf  # unendlich
+    # Anzahl der Leaves mit dem geringsten ErrAug ermitteln
+    depth = LowLeaves
+    while depth <= maxdepth[i]:
+        ErrAug = ErrIn[i, depth] + lambStar * depth
+        # Auswahl des Baumes mit dem geringsten ErrAug
+        if ErrAug < ErrAugStar:
+            ErrAugStar = ErrAug
+            depthstar[i] = depth
+
+        depth = depth + 1
+    i=i+1
+
 
 #Ermitteln des optimalen alpha
 lb=0                    #untere Grenze des Alpha
-hb=0.1                  #obere Grenze des Alpha
+hb=1                 #obere Grenze des Alpha
 StepSize=0.01           #erste Schrittgröße, in der das Alpha gesucht werden soll
 ErrCVStar=math.inf      #unendlich
 ErrCVCheck=math.inf     #unendlich
 alpha=lb                #Anfangswert des Alpha
 hbneu=hb
 epsilon=1
-j=0
-depthlamb={}
-while j<3:
+k=0
+while k<=3:
     #Durchgehen der einzelnen Lambdas und ermitteln der jeweiligen ErrCV
     while alpha <= hbneu:
-        if epsilon==0:
+        print(alpha)
+        if epsilon==1:
             Ralpha=alpha
         else:
             # Um nicht in lokalem Minimun zu landen Random Nummer aus alter Range
-            Ralpha=np.random.choice([alpha,random.choice(np.arange(lb,hb,StepSize))],p=[1-1/epsilon,1/epsilon])
+            Ralpha=np.random.choice([alpha,random.uniform(lb,hb)],p=[1-1/epsilon,1/epsilon])
         if Ralpha==alpha:
             alpha=alpha+StepSize
 
         if alpha!=0:
             ErrCV=0
             #Durchgehen der einzelnen Random Forests mit dem jeweiligen Alpha
-            for i in np.arange(0,nAlpha,1):
+            i=0
+            while i<nAlpha:
+                print('Alpha:',Ralpha,'/',hbneu,' by StepSize ',StepSize,',',i,'/',nAlpha)
                 Test = M[(N - KForest) - i*t:N - i * t + 1]         #Test=M[(N-KForest)-i*t:(N-KForest)-i*t+KForest+1]#Durchgehen der einzelnen Bäume des Random Forests zur Ermittlung des ErrCV
                 XTest = Test[Test.columns[:len(Test.columns) - 1]]  # Parameter des Testsets
                 YTest = Test[Test.columns[len(Test.columns) - 1]]   # Zielwert des Testsets
@@ -138,34 +184,48 @@ while j<3:
                 #Prediction des Testsets. Später die Maschine
                 #In For-Schleife vorhersage aus den einzelnen Bäumen, um Alpha diskontiert
                 #Endgültige Vorhersage mittels YPredictAlpha
-                for j in np.arange(0,nForest,1):
-                    YPredict = Alpha**j*Tree[j+i, depthstar[j+i]].predict(XTest) #Vorhersage aus Baum um Alpha diskontiert
+                j=0
+                while j<nForest:
+                    print('Alpha:',Ralpha,'/',hbneu,' by StepSize ',StepSize,',',i,'/',nAlpha,',',j,'/',nForest)
+                    YPredict = alpha**j*Tree[j+i, depthstar[j+i]].predict(XTest) #Vorhersage aus Baum um Alpha diskontiert
                     YPredictSum=YPredictSum+YPredict
-                    alphaSum=alphaSum+Alpha**j
+                    alphaSum=alphaSum+alpha**j
+                    j=j+1
 
                 YPredictAlpha=YPredictSum/alphaSum #Endgültige Vorhersage
 
                 #Ermitteln des ErrCV für das jeweilige Alpha
                 ErrCV=ErrCV+mean_squared_error(YPredictAlpha,YTest)
+                i=i+1
 
             #Speichern des Alpha mit dem geringsten ErrCV
-            if ErrCV<ErrCVStar:
+            if ErrCV/nAlpha<ErrCVStar:
                 alphaStar=alpha
-                ErrCVStar=ErrCV
+                ErrCVStar=ErrCV/nAlpha
 
     #Zähler, ob eine weitere Nachkommastelle sich lohnt
-    if ErrCVCheck - ErrCVStar < 0.00000001:
-        j = j + 1
-        ErrCVCheck=ErrCVStar
+    if ErrCVCheck - ErrCVStar < 0.1:
+        k = k + 1
     else:
-        j = 0
+        k = 0
+    ErrCVCheck = ErrCVStar
 
     #Anpassen der neuen Grenzen und der neuen StepSize
     alpha=max(0,alphaStar-StepSize)
     hbneu=alphaStar+StepSize
     StepSize = StepSize / 10
-    Epsilon=Epsilon+1
+    epsilon=epsilon+1
 
-print(lambStar)
-print(alphaStar)
-print(ErrCVStar)
+i=0
+while i<nTree:
+    dot_data = tree.export_graphviz(Tree[i, depthstar[i]], out_file = None, filled   = True, rounded  = True, special_characters = True)
+    graph = pydotplus.graph_from_dot_data(dot_data)
+    graph.write_pdf('FX Random Forest Tree {}.pdf'.format(i))
+    i=i+1
+print('LambdaStar: ',lambStar)
+print('Alpha Star: ',alphaStar)
+print('ErrCV Star:', ErrCVStar)
+print('Depth: ',depthstar)
+print('ErrIn:',ErrIn)
+print('MSE:', mse)
+print(time.time()-starttime)
--- Prepare_Data.py
+++ Prepare_Data.py
@@ -2,15 +2,20 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn import tree
 import pydotplus
+import time
+from time import strptime
 
+starttime=time.time()
 def prepare_igdata(symbol,start_date,end_date):
     Chead=["Date","Clock","Open","High","Low","Close","Volume"]
     df=pd.read_csv("{}.csv".format(symbol),sep=',',names=Chead,na_values=['nan'])
-    df['Time']=df[['Date','Clock']].apply(lambda x : '{} {}'.format(x[0],x[1]), axis=1)
+    df['Time']=df['Date'] + ' ' + df['Clock']
     df = df.set_index(['Time'])
-    df=df[['Clock','Open','High','Low','Close','Volume']]
+    df=df[['Open','High','Low','Close','Volume']]
     dates=pd.date_range(start_date,end_date, freq="1min")
     M=pd.DataFrame(index=dates)
+    Clock=M.index.hour+M.index.minute/60                #Convert clock string to float value
+    M['Clock']=pd.Series(Clock,index=M.index)
     M=M.join(df)
     M=M.dropna()
     M.index.names=['Date']
@@ -43,13 +48,13 @@ def RSI (M, wind):
     RSI.columns=['RSI']
     return RSI
 
-start_date='2017-09-24 00:00'
+start_date='2017-07-01 00:00'
 end_date='2017-09-30 23:59'
 prepared_igdata= prepare_igdata('USDZAR1Y',start_date,end_date)
-prepared_igdata.to_csv("USDZAR_prepared_IG.csv")
+prepared_igdata.to_csv("USDZAR1Y_prepared_IG.csv")
 Bollinger_Val=Bollinger_Value(prepared_igdata['Close'],20)
 HL_Val=HL_Value(prepared_igdata['Close'],1432)
-rets=(prepared_igdata['Close']/prepared_igdata['Open'])-1
+rets=((prepared_igdata['Close']/prepared_igdata['Open'])-1)*10000
 rets=rets.to_frame(name='Ret')
 RSI_Val=RSI(rets,14)
 Clock=prepared_igdata['Clock'].to_frame(name='Clock')
@@ -59,8 +64,38 @@ M=M.join(RSI_Val)
 M=M.join(rets)
 M['Ret']=M['Ret'].shift(-1)
 M=M[:-1]
-print(prepared_igdata)#only for test
-print(M)#only for test
 M=M.dropna()
-M.to_csv("USDZAR_prepared_Ind.csv")
+M.to_csv("USDZAR3M_prepared_Ind.csv")
+symbol='USDZAR'
+M.columns='{} '.format(symbol)+M.columns.values
+
+prepared_igdata= prepare_igdata('XAUUSD1Y',start_date,end_date)
+prepared_igdata.to_csv("XAUUSD1Y_prepared_IG.csv")
+Bollinger_Val=Bollinger_Value(prepared_igdata['Close'],20)
+HL_Val=HL_Value(prepared_igdata['Close'],1060)
+rets=((prepared_igdata['Close']/prepared_igdata['Open'])-1)*10000
+rets=rets.to_frame(name='Ret')
+RSI_Val=RSI(rets,14)
+Clock=prepared_igdata['Clock'].to_frame(name='Clock')
+M2=Clock.join(HL_Val)
+M2=M2.join(Bollinger_Val)
+M2=M2.join(RSI_Val)
+M2=M2.join(rets)
+M2=M2.dropna()
+M2.to_csv("XAUUSD3M_prepared_Ind.csv")
+symbol='XAUUSD'
+M2.columns='{} '.format(symbol)+M2.columns.values
+
+Mfinish=M[M.columns[:len(M.columns)-1]].join(M2[M2.columns[2:]])
+Mfinish=Mfinish.join(M[M.columns[len(M.columns)-1]])
+Mfinish.fillna(0,inplace=True)
+Mfinish.to_csv("USDZAR-XAUUSD3M_prepared_Ind.csv")
+print(Mfinish)
+
+totaltime=time.time()-starttime
+print(totaltime)
+
+
+
+
 
